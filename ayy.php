58f6e1b754d398f1a7281af0141b16905ac9f7b1
David Allen <david.allen@theresumator.com>
2015-10-15 13:16:41 -0400

    ok u win

--- ayy.php
+++ ayy.php
@@ -5,6 +5,5 @@ echo "ayy lmao\n";
 
 # dAYYvid aLAMOen
 echo "ayy lmao\n";
-echo " ( ͡~ ͜ʖ ͡°).\n";
 
 # Ur name here
---
+++ larry.php
@@ -0,0 +1,6 @@
+<?php
+
+# dAYYvid aLAMOen
+echo " ( ͡~ ͜ʖ ͡°).\n";
+echo " ( ͡° ͜ʖ ͡°)\n";
+echo " ᕦ( ͡°╭͜ʖ╮͡° )ᕤ\n";
